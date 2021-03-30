b27849c65e06715735ef91fbc2cf29bcb8478eb7
Keith Hand <keith@clevyr.com>
2021-03-30 17:06:54 -0500

    :building_construction: Functionality for action

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+import { getInput, setFailed, setOutput, info } from '@actions/core';
+import { execSync } from 'child_process';
+
+const PRE_RELEASE_KEY = 'pre-release';
+const NPM_BASE_COMMAND = 'npm run release';
 
 try {
-    console.log('Hello');
+    const preRelease = getInput(PRE_RELEASE_KEY);
+    let command = NPM_BASE_COMMAND;
+
+    if (preRelease) {
+        info(`${PRE_RELEASE_KEY} set, using ${preRelease} increment strategy`);
+        command += ` -- --preRelease=${preRelease}`;
+    } else {
+        info(`No ${PRE_RELEASE_KEY} set, using default increment strategy`);
+    }
+
+    execSync(command);
+    setOutput("version", newVersion);
 } catch (error) {
-    core.setFailed(error.message);
+    setFailed(error.message);
 }
