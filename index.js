932e865bbc941a863dec7a5f5c3f87b20d17c83d
Keith Hand <keith@clevyr.com>
2021-03-31 11:06:47 -0500

    :bug: Fixing import statements

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-import { getInput, setFailed, setOutput, info } from '@actions/core';
-import { execSync } from 'child_process';
+const core = require('@actions/core');
+const childProcess = require('child_process');
 
 const PRE_RELEASE_KEY = 'pre-release';
 const NPM_BASE_COMMAND = 'npm run release';
 
 try {
-    const preRelease = getInput(PRE_RELEASE_KEY);
+    const preRelease = core.getInput(PRE_RELEASE_KEY);
     let command = NPM_BASE_COMMAND;
 
     if (preRelease) {
-        info(`${PRE_RELEASE_KEY} set, using ${preRelease} increment strategy`);
+        core.info(`${PRE_RELEASE_KEY} set, using ${preRelease} increment strategy`);
         command += ` -- --preRelease=${preRelease}`;
     } else {
-        info(`No ${PRE_RELEASE_KEY} set, using default increment strategy`);
+        core.info(`No ${PRE_RELEASE_KEY} set, using default increment strategy`);
     }
 
-    execSync(command);
-    setOutput("version", newVersion);
+    childProcess.execSync(command);
+    core.setOutput("version", newVersion);
 } catch (error) {
-    setFailed(error.message);
+    core.setFailed(error.message);
 }
