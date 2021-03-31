d25c971367b8a357f0f1812be8985cc1413dbc6c
Keith Hand <keith@clevyr.com>
2021-03-31 15:07:29 -0500

    :wrench: Updating release-it config to be js file over json

---
+++ config.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const config = {
+    "npm": {
+        "publish": false,
+        "ignoreVersion": true
+    },
+    "git": {
+        "tagName": "v${version}"
+    },
+    "plugins": {
+        "@release-it/bumper": {
+            "in": {
+                "file": "VERSION",
+                "type": "text/plain"
+            },
+            "out": {
+                "file": "VERSION",
+                "type": "text/plain"
+            },
+        },
+    },
+}
+
+// Adds directory path for local plugin dynamically
+const calverPluginPath = `${__dirname}/clevyr-calver-plugin`
+config["plugins"][calverPluginPath] = {};
+
+module.exports = config;
+
