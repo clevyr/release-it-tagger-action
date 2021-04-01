c8600329d05f0e16b3a5116fc4d4cb0a3c45fd71
Keith Hand <keith@clevyr.com>
2021-04-01 14:34:31 -0500

    :children_crossing: Adding easier to customize input fields for action

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const core = require('@actions/core');
 const releaseIt = require('release-it');
 
-const PRE_RELEASE_KEY = 'pre-release';
+const GITHUB_REF_KEY = 'github-ref';
+const DEV_BRANCH_KEY = 'dev-branch';
+const STAGE_BRANCH_KEY = 'stage-branch';
+const PROD_BRANCH_KEY = 'prod-branch';
 
 function attachPlugins(pluginsArray) {
     const plugins = [
@@ -15,8 +18,26 @@ function attachPlugins(pluginsArray) {
     }
 }
 
+function preReleaseType() {
+    const githubRef = core.getInput(GITHUB_REF_KEY);
+    const devBranch = core.getInput(DEV_BRANCH_KEY);
+    const stageBranch = core.getInput(STAGE_BRANCH_KEY);
+    const prodBranch = core.getInput(PROD_BRANCH_KEY);
+
+    switch (githubRef) {
+        case devBranch:
+            return 'alpha';
+        case stageBranch:
+            return 'beta';
+        case prodBranch:
+            return false;
+        default:
+            return 'dev';
+    }
+}
+
 try {
-    const preRelease = core.getInput(PRE_RELEASE_KEY);
+    const preRelease = preReleaseType();
     const options = {
         'dry-run': true,
         'ci': true,
